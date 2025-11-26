7b5add74673665372f34c873033e4bfcc2b18abf
Avinash Reddy Thipparthi <57447641+avizway1@users.noreply.github.com>
2025-11-26 15:36:47 +0530

    instance count to 3

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_server" {
-  count         = 2
+  count         = 3
   ami           = "ami-03695d52f0d883f65"
   instance_type = "t3.micro"
   tags = {
