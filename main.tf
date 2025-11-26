0b4ffcff9efdb0689da4ff8319f785812a0746fa
Avinash Reddy Thipparthi <57447641+avizway1@users.noreply.github.com>
2025-11-26 15:39:47 +0530

    changed to instance 1

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_server" {
-  count         = 3
+  count         = 1
   ami           = "ami-03695d52f0d883f65"
   instance_type = "t3.micro"
   tags = {
