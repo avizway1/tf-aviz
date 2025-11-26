e0b62609332078f8ff3fa0200cfcee9d22083de9
Avinash Reddy Thipparthi <57447641+avizway1@users.noreply.github.com>
2025-11-26 16:53:45 +0530

    testing s3 block

--- main.tf
+++ main.tf
@@ -15,26 +15,10 @@ resource "aws_s3_bucket" "public_bucket" {
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.public_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-# 3. Apply a bucket policy to allow public read (s3:GetObject) access
-resource "aws_s3_bucket_policy" "public_read_policy" {
-  bucket = aws_s3_bucket.public_bucket.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = "*",
-        Action = "s3:GetObject",
-        Resource = "${aws_s3_bucket.public_bucket.arn}/*" # Grants access to all objects within the bucket
-      }
-    ]
-  })
-  # Ensure the public access block is applied first
-  depends_on = [aws_s3_bucket_public_access_block.public_access_block]
 }
