2bd8c2f96f4551eafcd1a718dfad5a76898171f1
Avinash Reddy Thipparthi <57447641+avizway1@users.noreply.github.com>
2025-11-26 15:23:52 +0530

    my first commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my_server" {
+  count         = 1
+  ami           = "ami-03695d52f0d883f65"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "aviz-tf-server"
+  }
+}
