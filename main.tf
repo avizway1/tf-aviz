0b497200d13a47a2802d56cc2989c3a2555725b2
Avinash <avizway@avizacademy.com>
2025-12-03 10:42:22 +0530

    new instance-sentinel test

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "my_server" {
   ami           = "ami-03695d52f0d883f65"
   instance_type = "t3.micro"
   tags = {
-    Name = "aviz-tf -lock-test"
+    Name = "aviz-tf-test"
   }
 }
