1009c6230875f772a6e988afc201e77fe0baf9ed
Avinash Reddy Thipparthi <57447641+avizway1@users.noreply.github.com>
2026-03-06 08:39:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "aws_ami" "al2023" {
 
 variable "insatnce_type" {
     type = string
-    default = "t3.micro"
+    default = "t3.small"
 }
 
 resource "aws_instance" "example" {
@@ -33,4 +33,31 @@ resource "aws_instance" "example" {
 
 output "ami_used" {
   value = data.aws_ami.al2023.id
+}
+
+
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "aviz-hcp-test-awar07"
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.public_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket     = aws_s3_bucket.public_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.this]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.public_bucket.arn}/*"
+    }]
+  })
 }
